971735a4254ee253ba39966e768768fb22f3cd1c
lvjianqun <lvjianqun2005@163.com>
2018-01-15 19:21:19 +0800

    vue-musie项目初始

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="app">
+    <action-sheet></action-sheet>
+  </div>
+</template>
+
+<script>
+import ActionSheet from './components/ActionSheet'
+
+export default {
+  name: 'App',
+  components: {
+    ActionSheet
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
