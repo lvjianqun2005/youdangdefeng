9d9f6d5453c4ab8146af8ed20c54ec0a2b49bdd4
lvjianqun <lvjianqun2005@163.com>
2018-01-16 16:02:54 +0800

    分支提交

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './style/reset.css';
+html{
+     background: rgb(0,0,0,0.5);
 }
 </style>
